5ab9c80711d1da2448823f250c42ffd0dddbbccb
Luiz Carlos Meira Jr <meiraj25@gmail.com>
2021-10-29 18:22:31 -0300

    AVANÇOS NO HTML, CSS E JS DO DÉCIMO TERCEIRO

--- site-calculadora/JS/decimo_terceiro.js
+++ site-calculadora/JS/decimo_terceiro.js
@@ -1,39 +1,71 @@
-///////////////                             ///////////////////
-///////////////         CALCULADORA        ///////////////////
-///////////////                             ///////////////////
+function calcular_decimo(){
+    var campo_salario_bruto = document.getElementById('campo_salario_bruto')
+    var campo_horas_extras = document.getElementById('campo_horas_extras')
+    var campo_meses_trabalhados = document.getElementById('campo_meses_trabalhados')
+    var campo_tipo_pagamento = document.getElementById('campo_tipo_pagamento')
 
-function calcular_liquido(){
-    var sal = document.getElementById('campo_salario_bruto')
-    var desc = document.getElementById('campo_descontos')
-    var valor_salario = Number(sal.value)
-    var valor_descontos = Number(desc.value)
+    var n1 = Number(campo_salario_bruto.value)
+    var n2 = Number(campo_horas_extras.value)
+    var n3 = Number(campo_meses_trabalhados.value)
+
+    var salario_bruto1 = document.getElementById('salario_bruto1')
+    var horas_extras1 = document.getElementById('horas_extras1')
     var inss1 = document.getElementById('inss1')
-    var inss2 = document.getElementById('inss2')
     var irrf1 = document.getElementById('irrf1')
+    var consolidado1 = document.getElementById('consolidado1')
+
+    var salario_bruto2 = document.getElementById('salario_bruto2')
+    var horas_extras2 = document.getElementById('horas_extras2')
+    var inss2 = document.getElementById('inss2')
     var irrf2 = document.getElementById('irrf2')
-    // DESCONTOS INSS
-    var desconto_7_5 = valor_salario / 100 * 7.5
-    var desconto_9 = valor_salario / 100 * 9
-    var desconto_12 = valor_salario / 100 * 12
-    var desconto_14 = valor_salario / 100 * 14
-    var irrf_7_5 = valor_salario / 100 * 7.5
-    var irrf_15 = valor_salario / 100 * 15
-    var irrf_22_5 = valor_salario / 100 * 22.5
-    var irrf_27_5 = valor_salario / 100 * 27.5
-    var desconto_inss1 = valor_salario - desconto_7_5
-    var desconto_inss2 = valor_salario - desconto_9
-    var desconto_inss3 = valor_salario - desconto_12
-    var desconto_inss4 = valor_salario - desconto_14
+    var consolidado2 = document.getElementById('consolidado2')
+
+    var salario_bruto3 = document.getElementById('salario_bruto3')
+    var horas_extras3 = document.getElementById('horas_extras3')
+    var inss3 = document.getElementById('inss3')
+    var irrf3 = document.getElementById('irrf3')
+    var consolidado3 = document.getElementById('consolidado3')
+    
+    /////// CÁLCULOS ///////
+    
+    var salario_bruto_div_12 = n1 / 12
+    var calculo_decimo_terceiro = salario_bruto_div_12 * n3
+    var total_decimo_terceiro = calculo_decimo_terceiro + n2
+
+    var desconto_7_5 = total_decimo_terceiro / 100 * 7.5
+    var desconto_9 = total_decimo_terceiro / 100 * 9
+    var desconto_12 = total_decimo_terceiro / 100 * 12
+    var desconto_14 = total_decimo_terceiro / 100 * 14
+
+    var irrf_7_5 = total_decimo_terceiro / 100 * 7.5
+    var irrf_15 = total_decimo_terceiro / 100 * 15
+    var irrf_22_5 = total_decimo_terceiro / 100 * 22.5
+    var irrf_27_5 = total_decimo_terceiro / 100 * 27.5
+
+    var desconto_inss1 = total_decimo_terceiro - desconto_7_5
+    var desconto_inss2 = total_decimo_terceiro - desconto_9
+    var desconto_inss3 = total_decimo_terceiro - desconto_12
+    var desconto_inss4 = total_decimo_terceiro - desconto_14
+
     var desconto_irrf1_inss2 = desconto_inss2 - irrf_7_5
     var desconto_irrf1_inss3 = desconto_inss3 - irrf_7_5
     var desconto_irrf2_inss3 = desconto_inss3 - irrf_15
     var desconto_irrf2_inss4 = desconto_inss4 - irrf_15
     var desconto_irrf3_inss4 = desconto_inss4 - irrf_22_5
     var desconto_irrf4_inss4 = desconto_inss4 - irrf_27_5
+
     var total_desconto_7_5 = desconto_7_5.toFixed(2)
     var total_desconto_9 = desconto_9.toFixed(2)
     var total_desconto_12 = desconto_12.toFixed(2)
     var total_desconto_14 = desconto_14.toFixed(2)
+    
+    var total_desconto_irrf1 = irrf_7_5.toFixed(2)
+    var total_desconto_irrf2 = irrf_15.toFixed(2)
+    var total_desconto_irrf3 = irrf_22_5.toFixed(2)
+    var total_desconto_irrf4 = irrf_27_5.toFixed(2)
+
+    
+
     var salario_c_desconto1 = desconto_inss1.toFixed(2)
     var salario_c_desconto2 = desconto_inss2.toFixed(2)
     var salario_9_7_5 = desconto_irrf1_inss2.toFixed(2)
@@ -42,10 +74,7 @@ function calcular_liquido(){
     var salario_14_15 = desconto_irrf2_inss4.toFixed(2)
     var salario_14_22_5 = desconto_irrf3_inss4.toFixed(2)
     var salario_14_27_5 = desconto_irrf4_inss4.toFixed(2)
-    var total_desconto_irrf1 = irrf_7_5.toFixed(2)
-    var total_desconto_irrf2 = irrf_15.toFixed(2)
-    var total_desconto_irrf3 = irrf_22_5.toFixed(2)
-    var total_desconto_irrf4 = irrf_27_5.toFixed(2)
+
     var in1 = '7,5%'
     var in2 = '9%'
     var in3 = '12%'
@@ -54,16 +83,21 @@ function calcular_liquido(){
     var ir2 = '15%'
     var ir3 = '22,5%'
     var ir4 = '27,5%'
-    var art2 = document.getElementById('resultados_salario_liquido')
-    var total_salario_liquido = document.getElementById('valor_salario_liquido')
+
+    var art2 = document.getElementById('resultados_decimo_terceiro')
+    var total_salario_liquido = document.getElementById('valor_decimo_terceiro')
     var referencias1 = document.getElementById('referencias')
         // DESCONTOS INSS
-    if (valor_salario <= 1100){
+    if (calculo_decimo_terceiro <= 1100){
+        salario_bruto2.innerText = `R$ ${calculo_decimo_terceiro}`
+        horas_extras2.innerText = `R$ ${n2}`
         inss1.innerText = `${in1}`
-        inss2.innerText = `R$ ${total_desconto_7_5}`
+        inss3.innerText = `R$ ${total_desconto_7_5}`
+        irrf1.innerHTML = '0%'
+        irrf3.innerHTML = '<strong>ISENTO</strong>'
+        consolidado2.innerText = `R$ ${total_decimo_terceiro}`
+        consolidado3.innerText = `R$ ${total_desconto_7_5}`
         total_salario_liquido.innerHTML = `<strong>R$ ${salario_c_desconto1}</strong>`
-        irrf1.innerHTML = '<strong>ISENTO</strong>'
-        irrf2.innerHTML = '<strong>ISENTO</strong>'
         art2.style.display = 'block'
         referencias1.style.top = '350px'
         // DESCONTOS INSS
@@ -131,26 +165,51 @@ function calcular_liquido(){
     }
 }
 
+
 ///////////////                             ///////////////////
 ///////////////            MOBILE           ///////////////////
 ///////////////                             ///////////////////
 
-function informar6(){
-    var informacao6 = document.getElementsByTagName('span')[0]
-    informacao6.style.display = 'block'
+function informar1(){
+    var informacao1 = document.getElementsByTagName('span')[0]
+    informacao1.style.display = 'block'
 }
-function informar7(){
-    var informacao7 = document.getElementsByTagName('span')[1]
-    informacao7.style.display = 'block'
+function informar2(){
+    var informacao2 = document.getElementsByTagName('span')[1]
+    informacao2.style.display = 'block'
+}
+function informar3(){
+    var informacao3 = document.getElementsByTagName('span')[2]
+    informacao3.style.display = 'block'
+}
+function informar4(){
+    var informacao4 = document.getElementsByTagName('span')[3]
+    informacao4.style.display = 'block'
+}
+function informar5(){
+    var informacao5 = document.getElementsByTagName('span')[4]
+    informacao5.style.display = 'block'
 }
 ////////////////////////////////////////////////////////////////
-function retirar_info6(){
-    var informacao6 = document.getElementsByTagName('span')[0]
-    informacao6.style.display = 'none'
+function retirar_info1(){
+    var informacao1 = document.getElementsByTagName('span')[0]
+    informacao1.style.display = 'none'
 }
-function retirar_info7(){
-    var informacao7 = document.getElementsByTagName('span')[1]
-    informacao7.style.display = 'none'
+function retirar_info2(){
+    var informacao2 = document.getElementsByTagName('span')[1]
+    informacao2.style.display = 'none'
+}
+function retirar_info3(){
+    var informacao3 = document.getElementsByTagName('span')[2]
+    informacao3.style.display = 'none'
+}
+function retirar_info4(){
+    var informacao4 = document.getElementsByTagName('span')[3]
+    informacao4.style.display = 'none'
+}
+function retirar_info5(){
+    var informacao5 = document.getElementsByTagName('span')[4]
+    informacao5.style.display = 'none'
 }
 
 ///////////////                             ///////////////////
